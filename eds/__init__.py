0655384e9170b3bdefeb2718d189d99752ad6897
Svenn-Arne Dragly <s@dragly.com>
2016-12-07 00:00:26 +0100

    Add require functions, read/write data

--- eds/__init__.py
+++ eds/__init__.py
@@ -1,6 +1,25 @@
+from __future__ import print_function, division, unicode_literals
+
 import os
 import yaml
 import numpy as np
+
+
+def _create_object_folder(folder, typename):
+    if os.path.exists(folder):
+        raise IOError("The folder '" + folder + "' already exists")
+    os.mkdir(folder)
+    meta_filename = _metafile_from_folder(folder)
+    with open(meta_filename, "w") as meta_file:
+        yaml.dump({"type": typename, "eds_version": 1},
+                  meta_file,
+                  default_flow_style=False,
+                  allow_unicode=True)
+
+
+def _metafile_from_folder(folder):
+    return os.path.join(folder, "meta.yml")
+
 
 class AttributeManager:
     def __init__(self, parent):
@@ -29,12 +48,16 @@ class AttributeManager:
     def attributes_filename(self):
         return self.parent.attributes_filename
 
-class Base:
-    def __init__(self, parent, name):
+
+class Object(object):
+    def __init__(self, parent, name, mode=None):
+        if mode is None:
+            if parent is None:
+                raise AttributeError("Both 'mode' and 'parent' cannot be None.")
+            mode = parent.mode
         self.parent = parent
         self.name = name
-        print("Base constructor")
-        pass
+        self.mode = mode
 
     @property   
     def attrs(self):
@@ -53,29 +76,59 @@ class Base:
     
     @property
     def meta_filename(self):
-        return os.path.join(self.folder, "meta.yml")
+        return _metafile_from_folder(self.folder)
 
 
-class Group(Base):
-    def __init__(self, parent, name):
-        super(Group, self).__init__(parent, name)
+class Group(Object):
+    def __init__(self, parent, name, mode=None):
+        super(Group, self).__init__(parent=parent, name=name, mode=mode)
         
     def create_dataset(self, name, data):
+        if name in self:
+            raise IOError("An object with name '" + name + "' already exists.")
+            
         dataset_folder = os.path.join(self.folder, name)
-        if os.path.exists(dataset_folder):
-            raise IOError("A dataset with the name " + name + "already exists")
-        os.mkdir(dataset_folder)
+        _create_object_folder(dataset_folder, "dataset")
         # TODO check dimensions, npy or npz
-        dataset = Dataset(self, name)
-        
-        with open(dataset.meta_filename, "w") as meta_file:
-            yaml.dump({"type": "dataset"},
-                      meta_file,
-                      default_flow_style=False,
-                      allow_unicode=True)
-                      
-        dataset._write(data)
+        dataset = Dataset(parent=self, name=name)
+        dataset.set_data(data)
         return dataset
+        
+    def create_group(self, name):
+        group_folder = os.path.join(self.folder, name)
+        _create_object_folder(group_folder, "group")
+        group = Group(parent=self, name=name)
+        return group
+        
+    def require_group(self, name):
+        if name in self:
+            current_object = self[name]
+            if isinstance(current_object, Group):
+                return current_object
+            else:
+                raise TypeError("An object with name '" + name + "' already "
+                                "exists, but it is not a Group.")
+        else: 
+            return create_group(name)
+            
+    def require_dataset(self, name, data):
+        if name in self:
+            current_object = self[name]
+            if isinstance(current_object, Dataset):
+                current_object.set_data(data)
+                return current_object
+            else:
+                raise TypeError("An object with name '" + name + "' already "
+                                "exists, but it is not a Group.")
+        else: 
+            return create_group(name)
+        
+    def __contains__(self, name):
+        folder = os.path.join(self.folder, name)
+        if os.path.exists(folder):
+            return True
+        else:
+            return False
         
     def __getitem__(self, name):
         folder = os.path.join(self.folder, name)
@@ -84,54 +137,58 @@ class Group(Base):
             meta_data = yaml.load(meta_file)
         if meta_data["type"] == "dataset":
             return Dataset(self, name)
+        elif meta_data["type"] == "group":
+            return Group(self, name)
         else:
             print("Data type", meta_data["type"])
             raise NotImplementedError("Only dataset implemented")
+    
+    def __setitem__(self, name, value):
+        current_item = self.__getitem__(name)
+        if isinstance(current_item, Dataset):
+            print("Is dataset")
+        else:
+            print("Data type")
+            raise NotImplementedError("Only dataset writing implemented")
 
 
 class File(Group):
     def __init__(self, folder, mode=None):
-        super(File, self).__init__(None, "__root__")
-        self.root_folder = folder
-        print("Opening", folder)
         if mode is None:
             mode = "a"
-            
+        super(File, self).__init__(parent=None, name="__root__", mode=mode)
+        self.root_folder = folder
+        
+        already_exists = os.path.exists(folder)
+        should_create_folder = False
+        
         if mode == "r":
-            # TODO open folder if exists, otherwise fail
-            pass
+            if not already_exists:
+                raise IOError("Folder " + folder + " does not exist.")
         elif mode == "r+":
-            # TODO open folder if exists, otherwise fail
-            pass
+            if not already_exists:
+                raise IOError("Folder " + folder + " does not exist.")
         elif mode == "w":
-            # TODO create folder or trunc if exists
-            pass
+            if already_exists:
+                raise NotImplementedError("Should remove folder, not implemented. Please remove manually.")
+            should_create_folder = True
         elif mode == "w-" or mode == "x":
-            # TODO create file, fail if exists
-            pass
+            if already_exists:
+                raise IOError("Folder " + folder + " already exists.")
+            should_create_folder = True
         elif mode == "a":
-            # TODO read/write if exists, create if doesn't exist
-            if os.path.exists(folder):
-                # TODO read folder structure and meta.yml
-                # TODO if meta.yml is missing, raise exception
-                pass
-            else:
-                # TODO create folder and meta.yml
-                os.mkdir(folder)
-                
-            with open(self.meta_filename, "w") as eds_config_file:
-                yaml.dump({"eds_version": 1, "type": "file"}, 
-                          eds_config_file,
-                          default_flow_style=False,
-                          allow_unicode=True)
+            should_create_folder = True
+        
+        if should_create_folder and not already_exists:
+            _create_object_folder(folder, "file")
                           
 
-class Dataset(Base):
-    def __init__(self, parent, name):
-        super(Dataset, self).__init__(parent, name)
+class Dataset(Object):
+    def __init__(self, parent, name, mode=None):
+        super(Dataset, self).__init__(parent=parent, name=name, mode=mode)
         self.data_filename = os.path.join(self.folder, "data.npy")
     
-    def _write(self, data):
+    def set_data(self, data):
         np.save(self.data_filename, data)
         
     def __getitem__(self, args):        
@@ -145,17 +202,25 @@ class Dataset(Base):
 if __name__ == "__main__":
     import shutil
     testfile = "/tmp/test.eds"
-    if os.path.exists(testfile):
-        shutil.rmtree(testfile)
+    # if os.path.exists(testfile):
+    #     shutil.rmtree(testfile)
     f = File(testfile)
     f.attrs["temperature"] = 99.0
     print(f.attrs["temperature"])
     
-    a = np.array([1, 2, 3, 4])
-    dset = f.create_dataset("mydata", a)
+    a = np.array([1, 2, 3])
+    dset = f.require_dataset("mydata", a)
     
-    print(dset.data)
+    print(f["mydata"][()])
     
     print(f["mydata"][2])
     
-    # TODO add dataset
+    group = f.require_group("mygroup")
+    
+    b = np.array([[1, 2, 3], [4, 5, 6]])
+    if "some_data" not in group:
+        dset2 = group.create_dataset("some_data", b)
+    else:
+        group["some_data"] = b
+        
+    print(group["some_data"][()])
