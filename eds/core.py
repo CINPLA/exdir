17925f2057cd6ae6d88166c207d705ed4d2077fb
Milad <miladh@ibv.uio.no>
2016-12-08 11:48:16 +0100

    Added todos and removed redundant code

--- eds/core.py
+++ eds/core.py
@@ -222,7 +222,10 @@ class File(Group):
                 raise IOError("Folder " + folder + " does not exist.")
         elif mode == "w":
             if already_exists:
-                shutil.rmtree(folder)
+                # shutil.rmtree(folder)
+                # TODO Need to check entire tree containes eds folders
+                # TODO Require force truncate
+                raise NotImplementedError("Cannot truncate file")
             should_create_folder = True
         elif mode == "w-" or mode == "x":
             if already_exists:
@@ -255,7 +258,7 @@ class Dataset(Object):
     def __setitem__(self, args, value):
         data = np.load(self.data_filename)
         data[args] = value
-        np.save(self.data_filename, data)
+        self.set_data(data)
         
     @property
     def data(self):
