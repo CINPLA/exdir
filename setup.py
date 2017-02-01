b2a5777501ab2314c7c42170b1105d2230fd7c96
Svenn-Arne Dragly <s@dragly.com>
2017-02-01 19:17:39 +0100

    Indentation setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ long_description = open("README.md").read()
 
 entry_points = None
 
-
 install_requires = []
 
-setup(name="exdir",
-      packages=find_packages(),
-      entry_points=entry_points,
-      include_package_data=True,
-      )
+setup(
+    name="exdir",
+    packages=find_packages(),
+    entry_points=entry_points,
+    include_package_data=True,
+)
