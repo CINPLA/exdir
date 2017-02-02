3cdd9f6e6482ca57b02b9fbcc7dd89165da21c4e
Svenn-Arne Dragly <s@dragly.com>
2017-02-02 18:32:43 +0100

    Add upload and install scripts

---
+++ libs/travis-conda-scripts/scripts/upload.sh
@@ -0,0 +1,9 @@
+conda install conda-build anaconda-client
+mkdir ~/conda-bld
+conda config --set anaconda_upload no
+export CONDA_BLD_PATH=~/conda-bld
+export VERSION=`date +%Y.%m.%d`
+conda build -c conda-forge .
+PACKAGE=$(conda build -c conda-forge . --output)
+echo "Package name $PACKAGE"
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u cinpla --force $PACKAGE
