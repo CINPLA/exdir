1308059e99fe2ac08f0490c47ef36a38ce7fdf5d
Svenn-Arne Dragly <s@dragly.com>
2017-02-03 13:41:54 +0100

    Add TRAVIS_PYTHON_VERSION

--- libs/travis-conda-scripts/scripts/upload.sh
+++ libs/travis-conda-scripts/scripts/upload.sh
@@ -4,7 +4,7 @@ mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export GIT_DESCRIBE=$(git describe --always --tags --long)
-conda build -c conda-forge .
+conda build . -c conda-forge --python $TRAVIS_PYTHON_VERSION
 PACKAGE=$(conda build -c conda-forge . --output)
 echo "Package name $PACKAGE"
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u cinpla --force $PACKAGE
