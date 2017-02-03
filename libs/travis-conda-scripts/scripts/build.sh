8e7989df634433b321a888569eb668d0fc8d3331
Svenn-Arne Dragly <s@dragly.com>
2017-02-03 16:42:50 +0100

    Add export

--- libs/travis-conda-scripts/scripts/build.sh
+++ libs/travis-conda-scripts/scripts/build.sh
@@ -1,7 +1,4 @@
-export PATH="$HOME/miniconda/bin:$PATH"
 conda install -y conda-build anaconda-client
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
-export CONDA_BLD_PATH=~/conda-bld
-export GIT_DESCRIBE=$(git describe --always --tags --long)
 conda build . -c conda-forge --python $TRAVIS_PYTHON_VERSION
--- libs/travis-conda-scripts/scripts/doctest.sh
+++ libs/travis-conda-scripts/scripts/doctest.sh
@@ -1,4 +1,3 @@
-export PATH="$HOME/miniconda/bin:$PATH"
 cd docs
 conda install sphinx
 make doctest
