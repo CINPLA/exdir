f22950132086dbc0a131293023f0ec5fdc9c0d8c
Svenn-Arne Dragly <s@dragly.com>
2017-02-03 11:37:51 +0100

    Run install before upload

--- libs/travis-conda-scripts/scripts/upload.sh
+++ libs/travis-conda-scripts/scripts/upload.sh
@@ -2,7 +2,6 @@ conda install -y conda-build anaconda-client
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
-export BUILD_VERSION=`date +%Y%m%d`
 conda build -c conda-forge .
 PACKAGE=$(conda build -c conda-forge . --output)
 echo "Package name $PACKAGE"
