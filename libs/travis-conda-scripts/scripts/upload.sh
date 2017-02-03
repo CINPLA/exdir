e5060cc440b06283e804a93f02ed05f0287f1761
Svenn-Arne Dragly <s@dragly.com>
2017-02-03 16:25:06 +0100

    Add doctest to travis

--- libs/travis-conda-scripts/scripts/upload.sh
+++ libs/travis-conda-scripts/scripts/upload.sh
@@ -1,10 +1,3 @@
-export PATH="$HOME/miniconda/bin:$PATH"
-conda install -y conda-build anaconda-client
-mkdir ~/conda-bld
-conda config --set anaconda_upload no
-export CONDA_BLD_PATH=~/conda-bld
-export GIT_DESCRIBE=$(git describe --always --tags --long)
-conda build . -c conda-forge --python $TRAVIS_PYTHON_VERSION
 PACKAGE=$(conda build -c conda-forge . --output)
 echo "Package name $PACKAGE"
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u cinpla --force $PACKAGE
