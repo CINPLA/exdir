ceee5e481ef661faee41f88a6a239a662589676c
Svenn-Arne Dragly <s@dragly.com>
2017-02-04 13:53:00 +0100

    Improve upload script

--- libs/travis-conda-scripts/scripts/upload.sh
+++ libs/travis-conda-scripts/scripts/upload.sh
@@ -1,4 +1,6 @@
 PACKAGE=$(conda build -c conda-forge . --output)
 echo "Package name $PACKAGE"
 set +x
+echo "Uploading to anaconda with anaconda upload..."
 anaconda -t "$CONDA_UPLOAD_TOKEN" upload -u cinpla --force $PACKAGE
+set -x
