577e1722aa48afad5bd5970ea4fd6295cc71cd88
Svenn-Arne Dragly <s@dragly.com>
2017-02-04 14:50:24 +0100

    Don't upload if travis failed

--- libs/travis-conda-scripts/scripts/upload.sh
+++ libs/travis-conda-scripts/scripts/upload.sh
@@ -1,6 +1,10 @@
-PACKAGE=$(conda build -c conda-forge . --output)
-echo "Package name $PACKAGE"
-set +x
-echo "Uploading to anaconda with anaconda upload..."
-anaconda -t "$CONDA_UPLOAD_TOKEN" upload -u cinpla --force $PACKAGE
-set -x
+if [ $TRAVIS_TEST_RESULT -eq 0 ]; then
+    PACKAGE=$(conda build -c conda-forge . --output)
+    echo "Package name $PACKAGE"
+    set +x
+    echo "Uploading to anaconda with anaconda upload..."
+    anaconda -t "$CONDA_UPLOAD_TOKEN" upload -u cinpla --force $PACKAGE
+    set -x
+else
+    echo "Upload cancelled due to failed test."
+fi
