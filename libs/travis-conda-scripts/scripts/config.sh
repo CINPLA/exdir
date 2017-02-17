c11247d48094f9cdd16221215dc403de3800c293
Svenn-Arne Dragly <s@dragly.com>
2017-02-17 14:54:48 +0100

    Add output about env variables, update meta.yaml

--- libs/travis-conda-scripts/scripts/config.sh
+++ libs/travis-conda-scripts/scripts/config.sh
@@ -1,3 +1,6 @@
 export PATH="$HOME/miniconda/bin:$PATH"
 export GIT_DESCRIBE=$(git describe --always --tags --long)
 export CONDA_BLD_PATH=/tmp/conda-bld
+echo PATH $PATH
+echo GIT_DESCRIBE $GIT_DESCRIBE
+echo CONDA_BLD_PATH $CONDA_BLD_PATH
