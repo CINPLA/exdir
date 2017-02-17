fde323a3366dc2539e69988fd182f4678b7613aa
Svenn-Arne Dragly <s@dragly.com>
2017-02-17 15:32:32 +0100

    Add fetch --unshallow for enough commits for tags
    see travis-ci/travis-ci#3412

--- libs/travis-conda-scripts/scripts/config.sh
+++ libs/travis-conda-scripts/scripts/config.sh
@@ -1,7 +1,8 @@
+git fetch --unshallow  # needed to get enough commits for tag, see travis-ci/travis-ci#3412
 export PATH="$HOME/miniconda/bin:$PATH"
 export GIT_DESCRIBE=$(git describe --always --tags --long)
 export GIT_TAG=$(git describe --always --tags --abbrev=0)
-export TAG_AND_TRAVIS_BUILD=${GIT_TAG}_build${TRAVIS_BUILD_NUMBER}
+export TAG_AND_TRAVIS_BUILD=${GIT_TAG}_b${TRAVIS_BUILD_NUMBER}
 export TAG_TRAVIS_BUILD_AND_COMMIT=${TAG_AND_TRAVIS_BUILD}_g${TRAVIS_COMMIT}
 export CONDA_BLD_PATH=/tmp/conda-bld
 echo PATH $PATH
