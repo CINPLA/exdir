f57efecdd337ef56e06be2a2b3b7b3db841c1ba0
Svenn-Arne Dragly <s@dragly.com>
2017-02-17 11:11:02 +0100

    Remove entrypoints

--- setup.py
+++ setup.py
@@ -8,14 +8,11 @@ import versioneer
 
 long_description = open("README.md").read()
 
-entry_points = None
-
 install_requires = []
 
 setup(
     name="exdir",
     packages=find_packages(),
-    entry_points=entry_points,
     include_package_data=True, 
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass()
