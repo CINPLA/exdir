61bb0d7f6b58838ab59f108cf2ea154cec58ff0a
Simen Tennøe <simetenn@gmail.com>
2017-06-06 10:47:11 +0200

    added h5py license and copyright statement

--- tests/test_object.py
+++ tests/test_object.py
@@ -1,3 +1,15 @@
+# This file is part of Exdir, the Experimental Directory Structure.
+#
+# Copyright 2017 Simen Tennøe
+#
+# License: MIT, see "LICENSE" file for the full license terms.
+#
+# This file contains code from h5py, a Python interface to the HDF5 library,
+# licensed under a standard 3-clause BSD license
+# with copyright Andrew Collette and contributors.
+# See http://www.h5py.org and the "3rdparty/h5py-LICENSE" file for details.
+
+
 import pytest
 import os
 import yaml
