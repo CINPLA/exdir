02945e7614ccc42c3eabb6ad24d5b82295045abc
Svenn-Arne Dragly <s@dragly.com>
2017-06-19 11:20:14 +0200

    Clean up imports

--- exdir/core/__init__.py
+++ exdir/core/__init__.py
@@ -3,14 +3,6 @@
 .. moduleauthor:: Svenn-Arne Dragly, Milad H. Mobarhan, Mikkel E. Lepperød
 """
 
-from __future__ import print_function, division, unicode_literals
-
-import os
-import numpy as np
-import shutil
-import quantities as pq
-import re
-
 from .exdir_object import Object
 from .exdir_file import File
 from .attribute import Attribute
