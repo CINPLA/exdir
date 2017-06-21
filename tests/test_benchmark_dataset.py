49e4ed40d756191662ba8c5d92fb8f5d730cb52b
Svenn-Arne Dragly <s@dragly.com>
2017-06-21 21:06:40 +0200

    Add benchmarks for object and datasets

---
+++ tests/test_benchmark_dataset.py
@@ -0,0 +1,68 @@
+import exdir
+import pytest
+import os
+import shutil
+import h5py
+import numpy as np
+
+
+def add_small_dataset(obj):
+    data = np.zeros((100, 100, 100))
+    obj.create_dataset("foo", data=data)
+    obj.close()
+
+
+def add_medium_dataset(obj):
+    data = np.zeros((1000, 100, 100))
+    obj.create_dataset("foo", data=data)
+    obj.close()
+
+
+def add_large_dataset(obj):
+    data = np.zeros((1000, 1000, 100))
+    obj.create_dataset("foo", data=data)
+    obj.close()
+
+
+def create_setup_h5py(tmpdir):
+    def setup():
+        testpath = str(tmpdir.mkdir("test").join("test.h5"))
+        if os.path.exists(testpath):
+            os.remove(testpath)
+        f = h5py.File(testpath)
+        return ((f,), {})
+    return setup
+
+
+def test_dataset_small_h5py(benchmark, tmpdir):
+    benchmark.pedantic(add_small_dataset, setup=create_setup_h5py(tmpdir))
+
+
+def test_dataset_medium_h5py(benchmark, tmpdir):
+    benchmark.pedantic(add_medium_dataset, setup=create_setup_h5py(tmpdir))
+
+
+def test_dataset_large_h5py(benchmark, tmpdir):
+    benchmark.pedantic(add_large_dataset, setup=create_setup_h5py(tmpdir))
+
+
+def create_setup_exdir(tmpdir):
+    def setup():
+        testpath = str(tmpdir.mkdir("test").join("test.exdir"))
+        if os.path.exists(testpath):
+            shutif.rmtree(testpath)
+        f = exdir.File(testpath)
+        return ((f,), {})
+    return setup
+
+
+def test_dataset_small_exdir(benchmark, tmpdir):
+    benchmark.pedantic(add_small_dataset, setup=create_setup_exdir(tmpdir))
+
+
+def test_dataset_medium_exdir(benchmark, tmpdir):
+    benchmark.pedantic(add_medium_dataset, setup=create_setup_exdir(tmpdir))
+
+
+def test_dataset_large_exdir(benchmark, tmpdir):
+    benchmark.pedantic(add_large_dataset, setup=create_setup_exdir(tmpdir))
