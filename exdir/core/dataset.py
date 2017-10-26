5057f52c587bdd7a3a7c548231b3ca095667454c
Svenn-Arne Dragly <s@dragly.com>
2017-10-26 16:18:01 +0200

    Remove final references to quantities in dataset.py

--- exdir/core/dataset.py
+++ exdir/core/dataset.py
@@ -2,9 +2,8 @@ import os
 import numpy as np
 import exdir
 
-import quantities as pq
-
 from . import exdir_object as exob
+
 
 def _prepare_write(data):
     attrs = {}
@@ -20,107 +19,10 @@ def _prepare_write(data):
 
     return data, attrs
 
-def convert_back_quantities(value):
-    """Convert quantities back from dictionary."""
-    result = value
-    if isinstance(value, dict):
-        if "unit" in value and "value" in value and "uncertainty" in value:
-            try:
-                result = pq.UncertainQuantity(value["value"],
-                                              value["unit"],
-                                              value["uncertainty"])
-            except Exception:
-                pass
-        elif "unit" in value and "value" in value:
-            try:
-                result = pq.Quantity(value["value"], value["unit"])
-            except Exception:
-                pass
-        else:
-            try:
-                for key, value in result.items():
-                    result[key] = convert_back_quantities(value)
-            except AttributeError:
-                pass
-
-    return result
-
-
-def convert_quantities(value):
-    """Convert quantities to dictionary."""
-
-    result = value
-    if isinstance(value, pq.Quantity):
-        result = {
-            "value": value.magnitude.tolist(),
-            "unit": value.dimensionality.string
-        }
-        if isinstance(value, pq.UncertainQuantity):
-            assert value.dimensionality == value.uncertainty.dimensionality
-            result["uncertainty"] = value.uncertainty.magnitude.tolist()
-    elif isinstance(value, np.ndarray):
-        result = value.tolist()
-    elif isinstance(value, np.integer):
-        result = int(value)
-    elif isinstance(value, np.float):
-        result = float(value)
-    else:
-        # try if dictionary like objects can be converted if not return the
-        # original object
-        # Note, this might fail if .items() returns a strange combination of
-        # objects
-        try:
-            new_result = {}
-            for key, val in value.items():
-                new_key = convert_quantities(key)
-                new_result[new_key] = convert_quantities(val)
-            result = new_result
-        except AttributeError:
-            pass
-
-    return result
 
 def _dataset_filename(dataset_directory):
     return dataset_directory / "data.npy"
 
-# def _create_dataset_directory(dataset_directory, data):
-#     exob._create_object_directory(dataset_directory, exob.DATASET_TYPENAME)
-#     filename = str(_dataset_filename(dataset_directory))
-#     np.save(filename, data)
-#     # dataset = exob.open_object(filename)
-#     # dataset._reset_data(data)
-#
-# def _extract_quantity(data):
-#     attrs = {}
-#     if isinstance(data, pq.Quantity):
-#         result = data.magnitude
-#         attrs["unit"] = data.dimensionality.string
-#         if isinstance(data, pq.UncertainQuantity):
-#             attrs["uncertainty"] = data.uncertainty
-#     else:
-#         result = data
-#     return attrs, result
-#
-# def _convert_data(data, shape, dtype, fillvalue):
-#     attrs = {}
-#     if data is not None:
-#         attrs, result = _extract_quantity(data)
-#         if not isinstance(result, np.ndarray):
-#             result = np.asarray(data, order="C", dtype=dtype)
-#
-#         if shape is not None and result.shape != shape:
-#             result = np.reshape(result, shape)
-#     else:
-#         if shape is None:
-#             result = None
-#         else:
-#             fillvalue = fillvalue or 0.0
-#             result = np.full(shape, fillvalue, dtype=dtype)
-#
-#     if result is None:
-#         raise TypeError("Could not create a meaningful dataset.")
-#
-#     return attrs, result
 
 class Dataset(exob.Object):
     """
