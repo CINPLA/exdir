72e7bbf424733ec35c4ab0823cc67a93ceda43dc
Svenn-Arne Dragly <s@dragly.com>
2017-11-07 14:31:04 +0100

    Update benchmarks

--- tests/test_benchmark_attributes.py
+++ tests/test_benchmark_attributes.py
@@ -2,21 +2,79 @@ import exdir
 import pytest
 import os
 import shutil
+import time
+import numpy as np
+import h5py
+
+def benchmark(name, target, setup=None, teardown=None, iterations=1):
+    total_time = 0
+    for i in range(iterations):
+        data = tuple()
+        if setup is not None:
+            data = setup()
+        start_time = time.time()
+        target(*data)
+        end_time = time.time()
+        total_time += end_time - start_time
+        if teardown is not None:
+            teardown(*data)
+    print("--------------------")
+    print("Result for:", name, sep="\n")
+    print("Iterations:", iterations, sep="\n")
+    print("Total:", total_time, sep="\n")
+    print("Mean:", total_time / iterations, sep="\n")
+    print("--------------------")
 
 
-def add_many_attributes(obj):
-    for i in range(30):
-        obj.attrs["hello" + str(i)] = "world"
+def create_setup_exdir():
+    def setup():
+        testpath = "/home/svenni/tmp/ramdisk/test.exdir"
+        # testpath = tmpdir / "test.exdir"
+        if os.path.exists(testpath):
+            shutil.rmtree(testpath)
+        f = exdir.File(testpath)
+        return f, testpath
+    return setup
 
 
-def test_benchmark_attribute_many_exdir(benchmark, exdir_tmpfile):
-    foo = exdir_tmpfile.create_group("foo")
-    benchmark(add_many_attributes, foo)
+def create_setup_h5py():
+    def setup():
+        testpath = "/home/svenni/tmp/ramdisk/test.h5"
+        # testpath = tmpdir / "test.h5"
+        if os.path.exists(testpath):
+            os.remove(testpath)
+        f = h5py.File(testpath)
+        return f, testpath
+    return setup
 
 
-def test_benchmark_attribute_many_h5py(benchmark, h5py_tmpfile):
-    foo = h5py_tmpfile.create_group("foo")
-    benchmark(add_many_attributes, foo)
+def benchmark_exdir(function, iterations=100):
+    benchmark(
+        "exdir_" + function.__name__,
+        lambda f, tmpdir: function(f),
+        create_setup_exdir(),
+        teardown_exdir,
+        iterations=iterations
+    )
+
+
+def benchmark_h5py(function, iterations=100):
+    benchmark(
+        "h5py_" + function.__name__,
+        lambda f, tmpdir: function(f),
+        create_setup_h5py(),
+        teardown_h5py,
+        iterations=iterations
+    )
+
+
+def teardown_exdir(f, testpath):
+    f.close()
+    shutil.rmtree(testpath)
+
+
+def teardown_h5py(f, testpath):
+    os.remove(testpath)
 
 
 def add_few_attributes(obj):
@@ -24,14 +82,9 @@ def add_few_attributes(obj):
         obj.attrs["hello" + str(i)] = "world"
 
 
-def test_benchmark_attribute_few_exdir(benchmark, exdir_tmpfile):
-    foo = exdir_tmpfile.create_group("foo")
-    benchmark(add_few_attributes, foo)
-
-
-def test_benchmark_attribute_few_h5py(benchmark, h5py_tmpfile):
-    foo = h5py_tmpfile.create_group("foo")
-    benchmark(add_few_attributes, foo)
+def add_many_attributes(obj):
+    for i in range(30):
+        obj.attrs["hello" + str(i)] = "world"
 
 
 def add_attribute_tree(obj):
@@ -48,12 +101,90 @@ def add_attribute_tree(obj):
     obj.attrs["test"] = tree
 
 
-def test_benchmark_attribute_tree_exdir(benchmark, exdir_tmpfile):
-    foo = exdir_tmpfile.create_group("foo")
-    benchmark(add_attribute_tree, foo)
+def add_small_dataset(obj):
+    data = np.zeros((100, 100, 100))
+    obj.create_dataset("foo", data=data)
+    obj.close()
 
 
-def test_benchmark_attribute_tree_h5py(benchmark, h5py_tmpfile):
-    foo = h5py_tmpfile.create_group("foo")
-    with pytest.raises(TypeError):
-        benchmark(add_attribute_tree, foo)
+def add_medium_dataset(obj):
+    data = np.zeros((1000, 100, 100))
+    obj.create_dataset("foo", data=data)
+    obj.close()
+
+
+def add_large_dataset(obj):
+    data = np.zeros((1000, 1000, 100))
+    obj.create_dataset("foo", data=data)
+    obj.close()
+
+
+def create_many_objects(obj):
+    for i in range(500):
+        group = obj.create_group("group{}".format(i))
+        data = np.zeros((10, 10, 10))
+        group.create_dataset("dataset{}".format(i), data=data)
+
+
+def iterate_objects(obj):
+    i = 0
+    for a in obj:
+        i += 1
+    return i
+
+
+def create_large_tree(obj, level=0):
+    if level > 4:
+        return
+    for i in range(3):
+        group = obj.create_group("group_{}_{}".format(i, level))
+        data = np.zeros((10, 10, 10))
+        group.create_dataset("dataset_{}_{}".format(i, level), data=data)
+        create_large_tree(group, level + 1)
+
+
+def test_benchmark_attributes(exdir_benchmark):
+    benchmarks = [
+        (add_few_attributes, 100),
+        (add_many_attributes, 10),
+        #(add_attribute_tree, 100),
+        (add_small_dataset, 100),
+        (add_medium_dataset, 10),
+        (add_large_dataset, 10),
+        (create_many_objects, 3),
+        (create_large_tree, 10),
+    ]
+
+    for function, iterations in benchmarks:
+        benchmark_exdir(function, iterations)
+        benchmark_h5py(function, iterations)
+
+    benchmark_exdir(add_attribute_tree)
+
+
+def test_benchmark_iteration():
+    def setup_exdir():
+        obj, path = create_setup_exdir()()
+        create_many_objects(obj)
+        return obj, path
+
+    def setup_h5py():
+        obj, path = create_setup_h5py()()
+        create_many_objects(obj)
+        return obj, path
+
+    benchmark(
+        "exdir_iteration",
+        lambda obj, path: iterate_objects(obj),
+        setup_exdir,
+        teardown_exdir,
+        iterations=2
+    )
+
+    benchmark(
+        "h5py_iteration",
+        lambda obj, path: iterate_objects(obj),
+        setup_h5py,
+        teardown_h5py,
+        iterations=2
+    )
--- tests/test_benchmark_dataset.py
+++ tests/test_benchmark_dataset.py
@@ -2,7 +2,9 @@ import exdir
 import pytest
 import os
 import shutil
+import timeit
 import h5py
+import time
 import numpy as np
 
 
@@ -23,46 +25,61 @@ def add_large_dataset(obj):
     obj.create_dataset("foo", data=data)
     obj.close()
 
-
-def create_setup_h5py(tmpdir):
-    def setup():
-        testpath = str(tmpdir.mkdir("test").join("test.h5"))
-        if os.path.exists(testpath):
-            os.remove(testpath)
-        f = h5py.File(testpath)
-        return ((f,), {})
-    return setup
-
-
-def test_dataset_small_h5py(benchmark, tmpdir):
-    benchmark.pedantic(add_small_dataset, setup=create_setup_h5py(tmpdir))
-
-
-def test_dataset_medium_h5py(benchmark, tmpdir):
-    benchmark.pedantic(add_medium_dataset, setup=create_setup_h5py(tmpdir))
-
-
-def test_dataset_large_h5py(benchmark, tmpdir):
-    benchmark.pedantic(add_large_dataset, setup=create_setup_h5py(tmpdir))
-
-
 def create_setup_exdir(tmpdir):
     def setup():
-        testpath = str(tmpdir.mkdir("test").join("test.exdir"))
+        testpath = "/home/svenni/tmp/ramdisk/test.exdir"
+        # testpath = tmpdir / "test.exdir"
         if os.path.exists(testpath):
             shutil.rmtree(testpath)
         f = exdir.File(testpath)
-        return ((f,), {})
+        return f, testpath
     return setup
 
+def create_setup_h5py(tmpdir):
+    def setup():
+        testpath = "/home/svenni/tmp/ramdisk/test.h5"
+        # testpath = tmpdir / "test.h5"
+        if os.path.exists(testpath):
+            os.remove(testpath)
+        f = h5py.File(testpath)
+        return f, testpath
+    return setup
 
-def test_dataset_small_exdir(benchmark, tmpdir):
-    benchmark.pedantic(add_small_dataset, setup=create_setup_exdir(tmpdir))
+def teardown_exdir(f, testpath):
+    f.close()
+    shutil.rmtree(testpath)
+
+def teardown_h5py(f, testpath):
+    os.remove(testpath)
+
+def test_benchmark_exdir_small(tmpdir, exdir_benchmark):
+    def target(f, testpath):
+        add_small_dataset(f)
+    exdir_benchmark("exdir_small", target, create_setup_exdir(tmpdir), teardown_exdir, iterations=100)
+
+def test_benchmark_h5py_small(tmpdir, exdir_benchmark):
+    def target(f, testpath):
+        add_small_dataset(f)
+    exdir_benchmark("h5py_small", target, create_setup_h5py(tmpdir), teardown_h5py, iterations=100)
+
+def test_benchmark_exdir_large(tmpdir, exdir_benchmark):
+    def target(f, testpath):
+        add_large_dataset(f)
+    exdir_benchmark("exdir_large", target, create_setup_exdir(tmpdir), teardown_exdir, iterations=10)
+
+def test_benchmark_h5py_large(tmpdir, exdir_benchmark):
+    def target(f, testpath):
+        add_large_dataset(f)
+    exdir_benchmark("h5py_large", target, create_setup_h5py(tmpdir), teardown_h5py, iterations=10)
 
 
-def test_dataset_medium_exdir(benchmark, tmpdir):
-    benchmark.pedantic(add_medium_dataset, setup=create_setup_exdir(tmpdir))
-
-
-def test_dataset_large_exdir(benchmark, tmpdir):
-    benchmark.pedantic(add_large_dataset, setup=create_setup_exdir(tmpdir))
+# def test_dataset_small_exdir(benchmark, tmpdir):
+#     benchmark.pedantic(add_small_dataset, setup=create_setup_exdir(tmpdir))
+#
+#
+# def test_dataset_medium_exdir(benchmark, tmpdir):
+#     benchmark.pedantic(add_medium_dataset, setup=create_setup_exdir(tmpdir))
+#
+#
+# def test_dataset_large_exdir(benchmark, tmpdir):
+#     benchmark.pedantic(add_large_dataset, setup=create_setup_exdir(tmpdir))
