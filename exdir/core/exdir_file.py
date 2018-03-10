dbd79be5f7fbb38d2dbbd994bae5076c42e1736e
Svenn-Arne Dragly <s@dragly.com>
2018-03-10 22:18:53 +0100

    Only validate name in File constructor if it is going to be created

--- exdir/core/exdir_file.py
+++ exdir/core/exdir_file.py
@@ -75,7 +75,6 @@ class File(Group):
             name_validation=name_validation,
             plugin_manager=plugin_manager
         )
-        self.name_validation(directory.parent, directory.name)
 
         already_exists = directory.exists()
         if already_exists:
@@ -116,6 +115,7 @@ class File(Group):
                 should_create_directory = True
 
         if should_create_directory:
+            self.name_validation(directory.parent, directory.name)
             exob._create_object_directory(directory, exob.FILE_TYPENAME)
 
     def close(self):
