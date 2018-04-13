8091ff0ce688e9e3a231a77ea4dae245eeb6bd7c
Milad <miladh@ibv.uio.no>
2018-04-13 14:14:48 +0200

    DOC: explicit path

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ import re
 import exdir
 
 import sys
-sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(0, os.path.abspath("../exdir/core"))
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if not on_rtd:  # only import and set the theme if we're building docs locally
