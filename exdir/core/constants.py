75ad0e83fd655777eed72e73b236056ae1889380
Simen Tennøe <simetenn@gmail.com>
2018-05-11 13:37:49 +0200

    BUG: added missing constants.py

---
+++ exdir/core/constants.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# metadata
+EXDIR_METANAME = "exdir"
+TYPE_METANAME = "type"
+VERSION_METANAME = "version"
+
+# filenames
+META_FILENAME = "exdir.yaml"
+ATTRIBUTES_FILENAME = "attributes.yaml"
+RAW_FOLDER_NAME = "__raw__"
+
+# typenames
+DATASET_TYPENAME = "dataset"
+GROUP_TYPENAME = "group"
+FILE_TYPENAME = "file"
