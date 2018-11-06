14a1223662cee06aee42878df993340e02f0472d
Svenn-Arne Dragly <s@dragly.com>
2018-11-06 20:34:42 +0100

    Add post/pre-link/unlink scripts to install nbextension files
    This also requires a dependency on notebook to work.

---
+++ .conda-recipe/post-link.sh
@@ -0,0 +1,1 @@
+"${PREFIX}/bin/jupyter-nbextension" enable exdir --py --sys-prefix >> "${PREFIX}/.messages.txt" 2>&1
---
+++ .conda-recipe/pre-unlink.sh
@@ -0,0 +1,1 @@
+"${PREFIX}/bin/jupyter-nbextension" uninstall exdir --py --sys-prefix >> "${PREFIX}/.messages.txt" 2>&1
